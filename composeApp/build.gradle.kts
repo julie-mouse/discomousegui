c7a2ef5290b261dd8514d6fed27fde63988b9140
Julie W <juliet@wel.day>
2025-04-06 18:33:29 -0400

    The version bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,7 +50,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Exe)
             packageName = "DiscomouseGUI"
-            packageVersion = "1.1.1"
+            packageVersion = "1.2.1"
 
             includeAllModules = true
         }
